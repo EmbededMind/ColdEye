0a4a3e2b0f5d941a27c5505ca54217f7246e1110
LiYi261x <li_yiemail@163.com>
2016-11-14 22:03:19 +0800

    下班型提交

--- ColdEye/File/RecordFileMetabolism.cpp
+++ ColdEye/File/RecordFileMetabolism.cpp
@@ -1,15 +1,96 @@
 #include "stdafx.h"
 #include "RecordFileMetabolism.h"
+#include "Database\DBShadow.h"
+#include "Pattern\Observer.h"
+#include <iostream> 
+#include <list>
 
 CRecordFileMetabolism::~CRecordFileMetabolism()
 {
 }
 
-void CRecordFileMetabolism::SetATrigger()
+bool CRecordFileMetabolism::SetATrigger()
 {
+	if (!AfxBeginThread(FileMetabolismThread, this))
+	{
+		return false;
+	}
+	return true;
 }
 
-bool CRecordFileMetabolism::DelFile(CFile & RecordFile)
+bool CRecordFileMetabolism::DelFile(CString DelPsth)
+{
+	WIN32_FIND_DATA findFileData;
+	HANDLE hfind = ::FindFirstFile(DelPsth, &findFileData);
+	if (hfind == INVALID_HANDLE_VALUE)
+	{
+		return false;
+	}
+	DeleteFile(DelPsth);
+	hfind = ::FindFirstFile(DelPsth, &findFileData);
+	if (hfind == INVALID_HANDLE_VALUE)
+	{
+		return true;
+	}
+	return false;
+}
+
+ULONGLONG CRecordFileMetabolism::KillAlarmFile()
+{
+	list<CRecordFileInfo*>::iterator iter = CDBShadow::GetInstance()->mAlarmFileInfoList.begin();
+	for (; iter != CDBShadow::GetInstance()->mAlarmFileInfoList.end(); ++iter)
+	{
+		if (!((*iter)->bIsLocked || (*iter)->bIsOccupied))
+		{
+			CTime time = (*iter)->tBegin;
+			CString FileName;
+			FileName.Format(_T("%d\\%d%02d%02d%02d%02d%02d.h264"), (*iter)->nOwner, time.GetYear(), time.GetMonth(),
+				time.GetDay(), time.GetHour(), time.GetMinute(), time.GetSecond());
+			if (DelFile(_T(ALARM_RECORD_PATH) + FileName))
+			{
+				ULONGLONG Size =  (*iter)->dlSize;
+				///////
+				return Size;
+			}
+		}
+	}
+	return false;
+}
+
+ULONGLONG CRecordFileMetabolism::KillNormalFile()
+{
+	list<CRecordFileInfo*>::iterator iter = CDBShadow::GetInstance()->mReocrdFileInfoList.begin();
+	for (; iter != CDBShadow::GetInstance()->mReocrdFileInfoList.end(); ++iter)
+	{
+
+	}
+	return INT64();
+}
+
+INT64 CRecordFileMetabolism::GetDiskFreeSpaceAsMB(CString DiskName)
+{
+	return 0;
+}
+
+bool CRecordFileMetabolism::IsTimeOutNormalFile()
 {
 	return false;
+}
+
+UINT CRecordFileMetabolism::FileMetabolismThread(LPVOID pParam)
+{
+	CRecordFileMetabolism *pRecordFileMetabolism = (CRecordFileMetabolism*)pParam;
+	pRecordFileMetabolism->SurplusSpaceNormal = pRecordFileMetabolism->GetDiskFreeSpaceAsMB(NORMALDISK);
+	pRecordFileMetabolism->SurplusSpaceAlarm = pRecordFileMetabolism->GetDiskFreeSpaceAsMB(ALARMDISK);
+	while( pRecordFileMetabolism->SurplusSpaceNormal < SURPLUSSPACENORMAL)
+	{
+	}
+	while (pRecordFileMetabolism->SurplusSpaceAlarm < SURPLUSSPACEALARM)
+	{
+	}
+	while (pRecordFileMetabolism->IsTimeOutNormalFile())
+	{
+	}
+	AfxEndThread(100);
+	return 0;
 }
--- ColdEye/File/RecordFileMetabolism.h
+++ ColdEye/File/RecordFileMetabolism.h
@@ -1,18 +1,30 @@
 #pragma once
 #include "stdafx.h"
-#include "File\RecordFileInfo.h"
+#include "File\RecordFileButler.h"
+#define SURPLUSSPACENORMAL 100
+#define SURPLUSSPACEALARM  100
+#define NORMALDISK _T("d")
+#define ALARMDISK  _T("e")
 class CRecordFileMetabolism
 {
 	~CRecordFileMetabolism();
 private:
 	CRecordFileMetabolism(){};
 public:
-	static CRecordFileMetabolism* getInstance() {
+	static CRecordFileMetabolism* GetInstance() {
 		static CRecordFileMetabolism instance;
 		return &instance;
 	};
-	void SetATrigger();
+	bool SetATrigger();
 private:
-	bool DelFile(CFile &RecordFile);
+	float SurplusSpaceNormal;
+	float SurplusSpaceAlarm;
+private:
+	bool DelFile(CString DelPsth);
+	ULONGLONG KillAlarmFile();
+	ULONGLONG KillNormalFile();
+	INT64 GetDiskFreeSpaceAsMB(CString DiskName);
+	bool IsTimeOutNormalFile();
+	static UINT FileMetabolismThread(LPVOID pParam);
 };
 
