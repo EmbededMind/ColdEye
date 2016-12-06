b32cfa1d7529015f3a4cb6596f51e6048d20a281
SealedGhost <815407069@qq.com>
2016-12-06 18:59:20 +0800

    nothing important.

--- ColdEye/Surface.h
+++ ColdEye/Surface.h
@@ -128,7 +128,6 @@ protected:
 	DECLARE_MESSAGE_MAP()
 
 public:
-	afx_msg void OnPaint();
 	afx_msg void OnSetFocus(CWnd* pOldWnd);
 	afx_msg void OnNcPaint();
 	afx_msg void OnKillFocus(CWnd* pNewWnd);
--- ColdEye/WallDlg.cpp
+++ ColdEye/WallDlg.cpp
@@ -72,6 +72,9 @@ bool CWallDlg::Ingest(CPort* pPort)
 	 mSurfaces[pos]->ShowWindow(SW_SHOW);
 
 
+	 mSurfaces[pos]->BindPort(pPort);
+	 mSurfaces[pos]->ExecuteConfig();
+
 	 DesignSurfaceLayout();
 	 ExecuteSurfaceLayout();
 
@@ -234,10 +237,14 @@ void CWallDlg::TestIngestOne()
 	Ingest(pNewPort);
 }
 
+
+
+
 void CWallDlg::TestSplitOne()
 {
 
 }
+
 
 
 void CWallDlg::DoDataExchange(CDataExchange* pDX)
